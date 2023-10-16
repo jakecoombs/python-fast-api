ec03895be360c07ae48155e20a961d51df03321a
jakecoombs <57802613+jakecoombs@users.noreply.github.com>
2023-10-16 22:49:05 +0100

    gitignore

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "hello world"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": {"posts": [{"id": 1, "message": "first post"}]}}
