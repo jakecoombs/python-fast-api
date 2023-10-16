00a23f459a17df3e72bbd585db76f013402073ed
jakecoombs <57802613+jakecoombs@users.noreply.github.com>
2023-10-17 00:06:17 +0100

    Add post request

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from random import randrange
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: int | None = None
+
+
+my_posts = [
+    {"id": 1, "title": "Title of post 1", "content": "Content of post 1"},
+    {"id": 2, "title": "Dog Breeds", "content": "Labs, Daschunds, ..."},
+]
 
 
 @app.get("/")
@@ -10,4 +26,18 @@ def read_root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": {"posts": [{"id": 1, "message": "first post"}]}}
+    """Get all posts."""
+    return {"data": my_posts}
+
+
+@app.post("/posts")
+def create_post(post: Post):
+    """Create a new post."""
+
+    # Create new post
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(3, 10000000)
+    my_posts.append(post_dict)
+
+    # Return created post
+    return {"data": post_dict}
